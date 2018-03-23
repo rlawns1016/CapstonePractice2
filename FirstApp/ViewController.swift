ecd2dcf19d705e8dea88fdb10c5338be5d63b268
Junho Kim <rlawns10164@gmail.com>
2018-03-23 14:32:31 +0900

    First Commit

--- FirstApp/ViewController.swift
+++ FirstApp/ViewController.swift
@@ -5,11 +5,14 @@
 //  Created by Release on 2018. 3. 21..
 //  Copyright © 2018년 Junho Kim. All rights reserved.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    let messageArray = ["May the force be with you", "Live long and prosper", "To infinity and beyond", "Space is big. You just won't believe how vastly, hugely, mindbogglinly big it is", 10] as [Any]
+    var index = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +24,15 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func doButtonTap(_ sender: UIButton) {
+        print("Button Touched")
+        if let nextString = self.messageArray[index] as? String{
+            self.messageLabel.text = nextString
+        }
+        self.messageLabel.adjustsFontSizeToFitWidth = true
+        
+        
+        index = (index + 1) % 5
+    }
 }
 
